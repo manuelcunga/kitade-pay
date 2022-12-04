d2d4f4415d6f585c19d09bbd202e2e0c671baa83
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-12-04 21:09:32 +0100

    add create user service

---
+++ src/modules/accounts/services/create/createUser.service.ts
@@ -0,0 +1,26 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UserRepository } from '../../../../infra/database/typeorm/repositories/users/UserRepositories';
+import { CreateUserDTO } from '../../dtos/createUserDTO';
+
+@Injectable()
+export class CreateUserService {
+  constructor(private createUserRepository: UserRepository) {}
+
+  async execute(data: CreateUserDTO) {
+    const alreadyUser = await this.createUserRepository.findByEmail(data.email);
+
+    if (alreadyUser) {
+      throw new BadRequestException('Este usuário já existe');
+    }
+
+    return await this.createUserRepository.create({
+      name: data.name,
+      email: data.email,
+      phone: data.phone,
+      password: data.password,
+      birth_date: data.birth_date,
+      createdAt: data.createdAt,
+      id: data.id,
+    });
+  }
+}
