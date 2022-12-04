29d39c7cd858e494fa0ab951476b1c121ff63411
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-12-04 21:05:01 +0100

    add main ts

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
+
+  const options = new DocumentBuilder()
+    .setTitle('MARKETEASY STORE REST API')
+    .setDescription('API MARKETEASY STORE')
+    .setVersion('1.0')
+    .addTag('app')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  await app.listen(3000);
+}
+
+bootstrap();
