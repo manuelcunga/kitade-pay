3877d9826cb01bf810350be262e4701c67a7d5a2
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-12-04 21:05:09 +0100

    add user test

---
+++ src/@core/domain/entities/User.spec.ts
@@ -0,0 +1,16 @@
+import { Users } from "./User";
+
+describe('User unit test', () => {
+  it('should create a new user', () => {
+    let data = new Date()
+    const user = new Users("Manuel", "manuel@gmail.com", 1234,"123456",data, "1234");
+    console.log(user)
+    expect(user.name).toBe("Manuel")
+    expect(user.email).toBe("manuel@gmail.com")
+    expect(user.phone).toBe(1234)
+    expect(user.password).toBe("123456")
+    expect(user.birth_date).toBe(data)
+    expect(user.id).toBe("1234")
+  });
+  
+});
