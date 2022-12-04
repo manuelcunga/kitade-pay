f75bc81f81193513b5930329eb507d9049603aff
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-12-04 21:05:33 +0100

    add Iuser repository

---
+++ src/@core/domain/interface/IuserRespository.ts
@@ -0,0 +1,11 @@
+import { UserTypeorm } from "src/infra/database/typeorm/entities/User-entities-typeorm";
+import { CreateUserDTO } from "src/modules/accounts/dtos/createUserDTO";
+import { Users } from "../entities/User";
+
+export interface IUserRepository {
+  create(data: Users): Promise<void>;
+  findByEmail(email: string): Promise<UserTypeorm>;
+  findAllUsers(): Promise<UserTypeorm[]>;
+  update(id: string, data: UserTypeorm): Promise<UserTypeorm>;
+  find(id: string): Promise<UserTypeorm>;
+}
