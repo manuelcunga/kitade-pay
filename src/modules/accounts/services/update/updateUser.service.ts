5e30ee4488dd31c8f0cfafe3f495c7d8694105cc
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-12-25 20:37:27 +0100

    refactor: :recycle: Update UserUpdate service

--- src/modules/accounts/services/update/updateUser.service.ts
+++ src/modules/accounts/services/update/updateUser.service.ts
@@ -1,13 +1,13 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Users } from 'src/@core/domain/entities/User';
 import { UserRepository } from 'src/infra/database/typeorm/repositories/users/UserRepositories';
-import { UpdateUserDTO } from '../../dtos/updateUser.dto';
+import { UpdateUsersDTO } from '../../dtos/updateUser.dto';
 
 @Injectable()
 export class UpdateUserService {
   constructor(private usersRepository: UserRepository) {}
 
-  async execute(id: string, data: UpdateUserDTO): Promise<Users> {
+  async execute(id: string, data: UpdateUsersDTO): Promise<Users> {
     const users = await this.usersRepository.update(id, data);
 
     if (!users) {
