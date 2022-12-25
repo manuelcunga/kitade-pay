889526897056fc7e2a14e28d406d1d2eb24fead0
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-12-25 20:36:37 +0100

    refactor: :recycle: Update  Updateuser controller

--- src/modules/accounts/controller/update/updateUser.controller.ts
+++ src/modules/accounts/controller/update/updateUser.controller.ts
@@ -3,19 +3,19 @@ import {
   Controller,
   Param,
   Put,
-  UsePipes,
-  ValidationPipe,
+  UseGuards,
 } from '@nestjs/common';
-import { UpdateUserDTO } from '../../dtos/updateUser.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { UpdateUsersDTO } from '../../dtos/updateUser.dto';
 import { UpdateUserService } from '../../services/update/updateUser.service';
 
-@Controller('users')
+@UseGuards(AuthGuard('jwt'))
+@Controller('api/v1/user/')
 export class UpdateUserController {
   constructor(private readonly usersService: UpdateUserService) {}
 
-  @UsePipes(ValidationPipe)
-  @Put(':id')
-  async handle(@Param('id') id: string, @Body() data: UpdateUserDTO) {
+  @Put('update/:id')
+  async handle(@Param('id') id: string, @Body() data: UpdateUsersDTO) {
     return this.usersService.execute(id, data);
   }
 }
