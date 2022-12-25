b8a1851821c99c945b166128eb6895bbf64d2d4d
Lazaro Manuell <manuelvianacunga@gmail.com>
2022-12-25 20:34:37 +0100

    refactor: :recycle: Update user repository interface

--- src/@core/domain/interface/IuserRespository.ts
+++ src/@core/domain/interface/IuserRespository.ts
@@ -1,11 +1,11 @@
 import { UserTypeorm } from "src/infra/database/typeorm/entities/User-entities-typeorm";
-import { UpdateUserDTO } from "src/modules/accounts/dtos/updateUser.dto";
+import { UpdateUsersDTO } from "src/modules/accounts/dtos/updateUser.dto";
 import { Users } from "../entities/User";
 
 export interface IUserRepository {
   create(data: Users): Promise<void>;
   findByEmail(email: string): Promise<UserTypeorm>;
   findAllUsers(): Promise<UserTypeorm[]>;
-  update(id: string, data: UpdateUserDTO): Promise<Users>;
+  update(id: string, data: UpdateUsersDTO): Promise<Users>;
   find(id: string): Promise<UserTypeorm>;
 }
